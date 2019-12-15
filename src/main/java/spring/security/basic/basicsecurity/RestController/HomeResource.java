3453731078206dd1b450bbdd67d26d01e69c1172
marek <marekgr7@gmail.com>
2019-12-15 07:36:37 +0100

    Basic authorization on hardcode user with roles added

--- src/main/java/spring/security/basic/basicsecurity/RestController/HomeResource.java
+++ src/main/java/spring/security/basic/basicsecurity/RestController/HomeResource.java
@@ -10,4 +10,15 @@ public class HomeResource {
         public String home(){
             return "<h1>Welcome</h1>";
         }
+
+        @GetMapping("/admin")
+        public String admin(){
+            return "Hello Admin";
+        }
+
+        @GetMapping("/user")
+        public String user(){
+        return "Hello user";
+        }
+
 }
--- src/main/java/spring/security/basic/basicsecurity/security/SecurityConfiguration.java
+++ src/main/java/spring/security/basic/basicsecurity/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package spring.security.basic.basicsecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,8 +19,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("Marek")
                 .password("1234")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("Majkel")
+                .password("1234")
+                .roles("ADMIN");
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+//                .antMatchers("/","static/css","static/js").permitAll()
+                //Allow all resources that can be accessed by everyone
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 
     // we shoud not be using NoOpPasswordEncoder - just to exercising
